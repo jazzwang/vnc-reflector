35e95a03f9ac7d2c97d3874bbad603a2b8fabcbb
const <const@f33e883d-de45-0410-ae52-dbc3c9ec18b3>
2001-08-04 22:06:53 +0000

    Version string changed.
    git-svn-id: https://svn.code.sf.net/p/vnc-reflector/code/trunk@36 f33e883d-de45-0410-ae52-dbc3c9ec18b3

--- reflector/reflector.h
+++ reflector/reflector.h
@@ -1,14 +1,14 @@
 /* VNC Reflector
  * Copyright (C) 2001 Const Kaplinsky
  *
- * $Id: reflector.h,v 1.4 2001/08/03 13:06:59 const Exp $
+ * $Id: reflector.h,v 1.5 2001/08/04 22:06:53 const Exp $
  * Global include file
  */
 
 #ifndef _REF_REFLECTOR_H
 #define _REF_REFLECTOR_H
 
-#define VERSION  "0.2"
+#define VERSION  "0.3"
 
 /* Framebuffer */
 extern RFB_SCREEN_INFO *g_screen_info;
