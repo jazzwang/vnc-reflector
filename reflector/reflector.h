8aa5936cf9a4d07c9451d37e8c564305b7dde755
const <const@f33e883d-de45-0410-ae52-dbc3c9ec18b3>
2001-10-10 11:43:27 +0000

    Version strings changed, version 1.1.4.
    git-svn-id: https://svn.code.sf.net/p/vnc-reflector/code/trunk@118 f33e883d-de45-0410-ae52-dbc3c9ec18b3

--- reflector/reflector.h
+++ reflector/reflector.h
@@ -10,14 +10,14 @@
  * This software was authored by Constantin Kaplinsky <const@ce.cctpu.edu.ru>
  * and sponsored by HorizonLive.com, Inc.
  *
- * $Id: reflector.h,v 1.18 2001/10/10 06:33:46 const Exp $
+ * $Id: reflector.h,v 1.19 2001/10/10 11:43:27 const Exp $
  * Global include file
  */
 
 #ifndef _REF_REFLECTOR_H
 #define _REF_REFLECTOR_H
 
-#define VERSION  "1.1.3"
+#define VERSION  "1.1.4"
 
 /* FIXME: Too many header files with too many dependencies */
 
